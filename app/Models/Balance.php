ea1ced9d7c99c34ae39365c2a0c37ab4d9211979
Mayke <mayke.suporte@gmail.com>
2018-03-27 18:07:21 -0400

    beginTransaction - Commit e Rollback

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Balance extends Model
 {
     public $timestamps = false;
 
     public function deposit(float $value) : Array{
+
+        DB::beginTransaction();
+
+
         $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.', '');
         $deposit= $this->save();
@@ -21,16 +26,25 @@ class Balance extends Model
             'date' => date('Ymd'),
         ]);
 
-        if ($deposit && $historic)
+        if ($deposit && $historic){
+
+            DB::commit();
+
             return[
                 'success' => true,
                 'message' => 'Sucesso ao recarregar'
             ];
+        } else {
 
-        return[
+            DB::rollback();
+            
+            return[
             'success' => false,
             'message' => 'Falha ao recarregar'
         ];
+        }
+
+        
 
 
 
