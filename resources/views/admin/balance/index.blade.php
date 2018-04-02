20f37824d7bc66de66d23a17648e9a413d615d15
Mayke <mayke.suporte@gmail.com>
2018-04-02 16:52:28 -0400

    #24 - Ajustes na view
    admin/balance/index

--- resources/views/admin/balance/index.blade.php
+++ resources/views/admin/balance/index.blade.php
@@ -16,27 +16,18 @@
         <div class = "box-header">
             <a href="{{ route('balance.deposit') }}" class="btn btn-primary"> <i class ="fa fa-cart-plus" aria-hidden="true"></i> Recarregar</a>
             <a href="" class="btn btn-danger">  <i class ="fa fa-cart-arrow-down" aria-hidden="true"></i> Sacar</a>
-
         </div>
 
-        <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box">
+        <div class="box-body">
+            @include('admin.includes.alerts')
+            <div class="info-box">
                 <span class="info-box-icon bg-aqua"><i class="ion ion-cash"></i></span>
-
-                <div class="info-box-content">
-                    @include('admin.includes.alerts')
+                <div class="info-box-content"> 
                     <span class="info-box-text">Saldo</span>
                     <span class="info-box-number">R$ {{ number_format($amount, 2, ',', '') }} </span>
                     <a href="#" class="small-box-footer">Ver histórico <i class ="glyphicon glyphicon-circle-arrow-right"></i></a>
                 </div>
-                <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-
-                
-            </div>
-        </div>
-        
-    </div>
+            </div> <!-- /.info-box-content -->
+        </div> <!-- /.info-box -->        
+    </div> <!-- /box -->
 @stop
