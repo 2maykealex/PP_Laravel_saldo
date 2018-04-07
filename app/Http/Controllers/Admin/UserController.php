bc57bf6e4820573e131628447e0788c582813823
Mayke <mayke.suporte@gmail.com>
2018-04-07 16:44:46 -0400

    #38 - Criar Template do Site no Laravel 5.5

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,4 +10,8 @@ class UserController extends Controller
     public function profile(){
         return view('site.profile.profile');
     }
+
+    public function profileUpdate(Request $request){
+        dd($request->all());
+    }
 }
--- resources/views/site/profile/profile.blade.php
+++ resources/views/site/profile/profile.blade.php
@@ -1,1 +1,36 @@
-#Profile
+@extends('site.layouts.app')
+
+@section('title', 'Meu perfil')
+
+@section('content')
+
+<h1>Meu perfil</h1>
+    
+<form action="{{ route('profile.update') }}" method="POST">
+    {!! csrf_field() !!}
+    <div class="form-group">
+        <label for="name">Nome</label>
+        <input type="text" name="name" value="{{ auth()->user()->name }}" placeholder="Nome:" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" name="email" value="{{ auth()->user()->email }}" placeholder="email:" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="password">Senha</label> 
+        <input type="password" name="password"  placeholder="Senha:" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="image">Imagem:</label>
+        <input type="file" name="image">
+    </div>
+
+    <div class="form-group">
+        <button type="submit" class="btn btn-info">Atualizar perfil</button>
+    </div>
+
+
+
+</form>
+
+@endsection
